067ef5aea28cd87eeeecc488c635e414e823d89c
Konrad Wasilewski <konradwasilewski@vp.pl>
2019-06-09 16:51:22 +0200

    komentarze do obiektu statycznego

--- PROJEKTLoty/ObjektStatyczny.cs
+++ PROJEKTLoty/ObjektStatyczny.cs
@@ -9,10 +9,17 @@ namespace PROJEKTLoty
 {
     class ObjektStatyczny
     {
+        // Objects position with z as its height
         private int x,z, y;
-       //kolor znaczka
         public int X { get=>x; private set =>x=value; }
         public int Y { get => y; private set => y = value; }
+
+        /// <summary>
+        /// Constructor of static object
+        /// </summary>
+        /// <param name="_z">Height of an object</param>
+        /// <param name="_x">X coordinate of an object</param>
+        /// <param name="_y">Y coordinate of an object</param>
         public ObjektStatyczny(int _z,int _x,int _y)
         {
             this.z = _z;
